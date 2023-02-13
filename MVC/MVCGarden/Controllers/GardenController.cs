508e5761ffd85359c7005eb36a3ef03c0837a959
Stefata05 <s.martinov2019@pmg-pernik.com>
2023-02-13 11:30:24 +0200

    add controller

---
+++ MVC/MVCGarden/Controllers/GardenController.cs
@@ -0,0 +1,23 @@
+﻿using MVCGarden.Model;
+using MVCGarden.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Controllers
+{
+     public class GardenController
+     {
+        private Display display;
+        private Garden garden;
+        public GardenController()
+        {
+            display = new Display();
+            garden = new Garden(display.CenaZelenchuci,display.CenaPlodove,display.KgZelenchuci,display.KgPlodove);
+            display.Rezultat = garden.KalkuliraneNaSuma();
+            display.VijdaneNaRezultat();
+        }
+     }
+}
